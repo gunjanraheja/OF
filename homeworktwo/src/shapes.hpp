e84e613305d83a1d4f7179d0fc4eaefbe442cca5
gunjanraheja <raheg611@newschool.edu>
2016-02-10 01:03:10 -0500

    homework

---
+++ homeworktwo/src/shapes.hpp
@@ -0,0 +1,37 @@
+//
+//  shapes.hpp
+//  week2_Homework
+//
+//  Created by gunjan deepak raheja on 2/9/16.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+#include <stdio.h>
+
+
+
+
+
+class shapes{
+public:
+    // all variables
+    int sides;
+    int vertices;
+    
+    
+    
+    
+    
+    
+    
+    // methods
+    void drawTriangle();
+    void drawSquare();
+    void drawCircle();
+    
+    
+};
